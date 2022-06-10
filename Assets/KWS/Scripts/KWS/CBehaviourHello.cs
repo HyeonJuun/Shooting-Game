61c6f004019ea4f622e9412f486b0b19921fee2d
xrispkws <106509595+xrispkws@users.noreply.github.com>
2022-06-10 10:22:27 +0900

    Add cs
    CBehaviourHello 스크립트에 추가 코드를 작성함

--- Assets/KWS/Scripts/KWS/CBehaviourHello.cs
+++ Assets/KWS/Scripts/KWS/CBehaviourHello.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class CBehaviourHello : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Hello");
     }
 }
